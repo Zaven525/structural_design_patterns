27f8058c5bf6d22722f29fc0701001ff59516ef9
Zaven525 <71457724+Zaven525@users.noreply.github.com>
2025-07-25 18:28:34 +0400

    Add files via upload

--- decorator.cpp
+++ decorator.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
+#include <cctype>
+#include <memory>
 
 class Message {
 public:
@@ -14,12 +16,35 @@ public:
 
 class Decorator : public Message {
 protected:
-    Message *message;
+    std::unique_ptr<Message> message;
 };
 
 class UpperCaseDecorator : public Decorator{
-    return 
+    std::string getText(const std::string& message) const override { 
+        std::string tmp_message = message;
+        for (int i = 0; i < tmp_message.size(); i++) {
+            tmp_message[i] = toupper(tmp_message[i]);
+        }
+    }
 };
+
+class LowerCaseDecorator : public Decorator {
+    std::string getText(const std::string& message) const override {
+        std::string tmp_message = message;
+        for (int i =0; i < tmp_message.size(); i++) {
+            tmp_message[i] = tolower(tmp_message[i]);
+        }
+    }
+};
+
+class EncryptedMessageDecorator : public Decorator {
+public:
+    EncryptedMessageDecorator(std::unordered_map<char &a, char &b>) {}
+private:
+    std::unordered_map ;
+};
+
+
 int main() {
 
     return 0;
